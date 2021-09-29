1a96b5edaa2801d30b3875157b002d2b2c37b305
JoYoungWook 조영욱 <siosio24@naver.com>
2021-09-29 16:42:36 +0900

    index.html 修正

--- src/main/java/com/rbwsn/controller/IndexController.java
+++ src/main/java/com/rbwsn/controller/IndexController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.jws.WebParam;
 import javax.validation.Valid;
 
 @Controller
